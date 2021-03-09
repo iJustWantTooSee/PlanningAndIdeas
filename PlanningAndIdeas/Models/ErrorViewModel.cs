11b27b2eb9a380b35e5a713af6fa459bcb7aa026
iJustWantTooSee <viktor.skachkov.tomsk@mail.ru>
2021-03-10 00:01:34 +0700

    initial commit

---
+++ PlanningAndIdeas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PlanningAndIdeas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PlanningAndIdeas/gulpfile.js
@@ -0,0 +1,38 @@
+﻿const gulp = require("gulp");
+const concat = require("gulp-concat");
+const sass = require("gulp-sass");
+
+const vendorStyles = [
+    "node_modules/bootstrap/dist/css/bootstrap.min.css"
+];
+const vendorScripts = [
+    "node_modules/jquery/dist/jquery.min.js",
+    "node_modules/popper.js/dist/umd/popper.min.js",
+    "node_modules/bootstrap/dist/js/bootstrap.min.js",
+];
+
+gulp.task("build", ["build-vendor", "sass"]);
+
+gulp.task("build-vendor", ["build-vendor-css", "build-vendor-js"]);
+
+gulp.task("build-vendor-css", () => {
+    return gulp.src(vendorStyles)
+        .pipe(concat("vendor.min.css"))
+        .pipe(gulp.dest("wwwroot/css"));
+});
+
+gulp.task("build-vendor-js", () => {
+    return gulp.src(vendorScripts)
+        .pipe(concat("vendor.min.js"))
+        .pipe(gulp.dest("wwwroot/scripts"));
+});
+
+gulp.task("sass", function () {
+    return gulp.src("wwwroot/css/**/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest("wwwroot/css"));
+});
+
+gulp.task("sass:watch", function () {
+    gulp.watch("wwwroot/styles/**/*.scss", ["sass"]);
+});
